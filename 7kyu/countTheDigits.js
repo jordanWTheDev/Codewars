d6e3c66cc070e5230f042a194356c0e479d92371
jordanWTheDev <jordancodywerner@gmail.com>
2022-06-14 21:27:32 -0500

    Count the Digit

---
+++ 7kyu/countTheDigits.js
@@ -0,0 +1,49 @@
+// Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+
+// Square all numbers k (0 <= k <= n) between 0 and n.
+
+// Count the numbers of digits d used in the writing of all the k**2.
+
+// Call nb_dig (or nbDig or ...) the function taking n and d as parameters and returning this count.
+
+// Examples:
+// n = 10, d = 1 
+// the k*k are 0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100
+// We are using the digit 1 in: 1, 16, 81, 100. The total count is then 4.
+
+// nb_dig(25, 1) returns 11 since
+// the k*k that contain the digit 1 are:
+// 1, 16, 81, 100, 121, 144, 169, 196, 361, 441.
+// So there are 11 digits 1 for the squares of numbers between 0 and 25.
+
+
+//My messy answer
+
+function nbDig(n, d) {
+    let nums = []
+    var count = 0;
+    //Adds each square up to the target number to an array
+    for(var i = 0; i <= n; i++) {
+      nums.push(Math.pow(i, 2).toString())
+    }
+    //Breaks up each number into seperate digits. This creates a list inside of a list
+    nums = nums.map(num => num.split(''))
+    //Iterates through each list within a list and if any number matches d, it adds 1 to the count variable
+    nums.forEach(num => {
+      num.forEach(num => {
+        if (num === d.toString()){
+          count += 1
+        }
+      })
+    })
+    return count
+}
+
+//Better answer
+function nbDig(n, d) {
+    var res=0;
+        for (var g=0;g<=n;g++){
+          var square=(g*g+"").split("");
+          square.forEach((s)=>s==d?res++:null)
+        }return res;
+    }
